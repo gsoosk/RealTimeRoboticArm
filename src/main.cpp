50f4166bbf442efd88aa96697dec1865620d7ef6
Farzad Habibi <farzadhabibii98@gmail.com>
2019-04-25 22:10:01 +0430

    Add max and min microsecond for servo 0, 1, 2"

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,28 @@
 #include <Arduino.h>
+#include <Servo.h>
+#define SERVO_0_MIN_MICROSECOND 590
+#define SERVO_0_MAX_MICROSECOND 2245
+#define SERVO_1_MIN_MICROSECOND 800
+#define SERVO_1_MAX_MICROSECOND 2400
+#define SERVO_2_MIN_MICROSECOND 675
+#define SERVO_2_MAX_MICROSECOND 2525
+//TODO: Adding servo 3 min and max microsecond 
 
+
+
+Servo servo;
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  // default values for attach are (544 , 2400)
+  servo.attach(9, SERVO_1_MIN_MICROSECOND, SERVO_1_MAX_MICROSECOND); // defaults are 544 - 2400 to 
+  Serial.begin(9600);
+  servo.write(90);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  while ( Serial.available() )
+  {
+    String x = Serial.readString();
+    servo.write(x.toInt());
+  }
+  
 }
