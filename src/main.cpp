20ae43dc0de7230f7e13d3f5250d4a32083851da
Farzad Habibi <farzadhabibii98@gmail.com>
2019-05-09 19:32:34 +0430

    Change timers to one because it can be handled in one task

--- src/main.cpp
+++ src/main.cpp
@@ -29,19 +29,15 @@
 
 
 #define ENDLESS_TIMER 0
-#define TIMER_PERIOD 40 // In mili seconds Second = 25 Hz
+#define TIMER_PERIOD 40 // In Milis Second = 25 Hz
 
-void readAndWrite0();
-void readAndWrite1();
-void readAndWrite2();
+void readAndWrite();
 
 Servo servo0;
 Servo servo2;
 Servo servo1;
 
-Ticker timer0(readAndWrite0, TIMER_PERIOD, ENDLESS_TIMER, MILLIS);
-Ticker timer1(readAndWrite1, TIMER_PERIOD, ENDLESS_TIMER, MILLIS);
-Ticker timer2(readAndWrite2, TIMER_PERIOD, ENDLESS_TIMER, MILLIS);
+Ticker timer(readAndWrite, TIMER_PERIOD, ENDLESS_TIMER, MILLIS);
 
 void setup() {
   // default values for attach are (544 , 2400)
@@ -52,29 +48,19 @@ void setup() {
   pinMode(A1, INPUT);
   pinMode(A0, INPUT);
 
-  timer0.start();
-  timer1.start();
-  timer2.start();
+  timer.start();
 }
 
 void loop() {
-  timer0.update();
-  timer1.update();
-  timer2.update();
+  timer.update();
 }
 
-void readAndWrite0()
+void readAndWrite()
 {
   int x0 = map(analogRead(A0), POT_0_MAX, POT_0_MIN, SERVO_MIN, SERVO_MAX);
   servo0.write(x0);
-}
-void readAndWrite1()
-{
   int x1 = map(analogRead(A1), POT_1_MAX, POT_1_MIN, SERVO_MIN, SERVO_MAX);
   servo1.write(x1);
-}
-void readAndWrite2()
-{
   int x2 = map(analogRead(A2), POT_2_MAX, POT_2_MIN, SERVO_MIN, SERVO_MAX);
   servo2.write(x2);
 }
