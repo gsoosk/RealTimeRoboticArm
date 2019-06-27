071a94fa4be01bc9788c78b777a754510b5548dc
Yasaman <ys.jafari98@gmail.com>
2019-06-27 15:31:06 +0430

    Add support for 25hz frequency

--- src/main.cpp
+++ src/main.cpp
@@ -50,25 +50,24 @@ void setup() {
 
   cli();//stop interrupts
 
-  //set timer0 interrupt at 2kHz
-  TCCR0A = 0;// set entire TCCR0A register to 0
-  TCCR0B = 0;// same for TCCR0B
-  TCNT0  = 0;//initialize counter value to 0
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
   // set compare match register for 25hz increments
-  OCR0A = 9999;// = (16*10^6) / (25*64) - 1 (must be <256)
+  OCR1A = 9999;// = (16*10^6) / (25*1024) - 1 (must be <65536)
   // turn on CTC mode
-  TCCR0A |= (1 << WGM01);
-  // Set CS01 and CS00 bits for 64 prescaler
-  TCCR0B |= (1 << CS01) | (1 << CS00);   
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
   // enable timer compare interrupt
-  TIMSK0 |= (1 << OCIE0A);
+  TIMSK1 |= (1 << OCIE1A);
 
   sei();//allow interrupts
 
   // timer.start();
 }
 
-ISR(TIMER0_COMPA_vect){//timer0 interrupt 2kHz toggles pin 8
+ISR(TIMER1_COMPA_vect){//timer0 interrupt 2kHz toggles pin 8
 //generates pulse wave of frequency 2kHz/2 = 1kHz (takes two cycles for full wave- toggle high then toggle low)
   readAndWrite();
 }
